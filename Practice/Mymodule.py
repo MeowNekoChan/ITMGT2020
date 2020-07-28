35eda2686cdf4eba3ba612a9c554c3a8bbc07672
MeowNekoChan <67996260+MeowNekoChan@users.noreply.github.com>
2020-07-28 16:52:42 +0800

    Lab2 Assignment yeah

---
+++ Practice/Mymodule.py
@@ -0,0 +1,4 @@
+def greeting():
+    return "Hello World!"
+def second_greeting():
+    return "How are you?"
---
+++ Practice/coffeepython.py
@@ -0,0 +1,7 @@
+def regular_price(price):
+    return price
+
+
+def sc_price(price):
+    discount = .20
+    return price*(1-discount)
